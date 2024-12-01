f3a3aaebe7b3af32f4c27c1262227f0922503e3b
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-12-01 21:04:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const imageUrls = [
   { url: 'https://picsum.photos/id/237/200/300' },
-  { url: 'https://picsum.photos/id/237/200/300' },
-  { url: 'https://picsum.photos/id/237/200/300' },
+  { url: 'https://picsum.photos/id/238/200/300' },
+  { url: 'https://picsum.photos/id/239/200/300' },
 ];
 
 function downloadImage(image) {
